7c455796468bf4fede7eb780f714f1d6bd76b527
netoADS <jc.neto1999@gmail.com>
2025-06-16 03:23:40 -0300

    Corrige base no vite.config.js 2

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-  return {
-    plugins: [react()],
-    base: mode === 'development' ? '/' : '/escola-capoeira/', // '/' para dev, '/escola-capoeira-orisun/' para build
-    build: {
-      assetsInlineLimit: 0,
-      rollupOptions: {
-        output: {
-          entryFileNames: `assets/[name]-[hash].js`,
-          chunkFileNames: `assets/[name]-[hash].js`,
-          assetFileNames: `assets/[name]-[hash].[ext]`,
-        },
-      },
-    },
-  };
+export default defineConfig({
+  plugins: [react()],
+  base: '/',
 });
